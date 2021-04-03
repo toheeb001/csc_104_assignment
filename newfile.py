e0402751b9aa10843e2e1e8ed4a90253203a1dcf
toheeb001 <81780541+toheeb001@users.noreply.github.com>
2021-04-03 20:11:12 +0100

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,13 @@
+name = "Toheeb"
+dept = "science education"
+print = (name, "/n", dept)
+scare = "boo!"
+print (scare)
+n1 = 3
+n2 = 5
+n3 = n1 + n2
+print ("n1 =", n1,"\nn2 =", n2,"\nn3 = n1 + n2","nn3 =", n1 + n2","\n")
+rem = 8/0.5
+print (rem)
+x = (2*4)*(4+2)
+print (x)
